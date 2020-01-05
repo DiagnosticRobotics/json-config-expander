0e7f97270069c7808ae9ff7053566bf409cbce5b
Alon <alonl@>
2020-01-05 09:08:42 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
 	name="json-config-expander",
-	version="0.4",
+	version="0.6",
 	author="Diagnostic Robotics",
 	author_email="admin@diagnosticrobotics.com",
 	description="Expand multi optional configuration to multiple configurations.",
